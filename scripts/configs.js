2ae84a3df857a7653782485395b93981bca05418
deck-adobe <deckreyes@adobe.com>
2024-09-30 10:58:33 -0700

    Check for aemxwalk

--- scripts/configs.js
+++ scripts/configs.js
@@ -32,9 +32,9 @@ export const calcEnvironment = () => {
 
 function buildConfigURL(environment) {
   const env = environment || calcEnvironment();
-  // let fileName = 'configs.json?sheet=prod';
-  let fileName = 'configs.json';
-  //let fileName = 'configs.json?sheet=prod';
+  if (aemxwalk === 'true'){let fileName = 'configs.json'; } else {
+    let fileName = 'configs.json?sheet=prod';
+  }
   if (env !== 'prod') {
     fileName = `configs-${env}.json`;
   }
