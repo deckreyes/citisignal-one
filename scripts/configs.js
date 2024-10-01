8da515014c9bd73f199ed4a111874f465f1455c4
deck-adobe <deckreyes@adobe.com>
2024-10-01 16:47:57 -0700

    Debugging configs.

--- scripts/configs.js
+++ scripts/configs.js
@@ -2,11 +2,7 @@ const ALLOWED_CONFIGS = ['prod', 'stage', 'dev'];
 
 // Added to check if doc or xwalk.
 //const isAemAuthor = getAemAuthorEnv(); 
-if (!isAemAuthor) {
-//import { getMetadata } from '../../scripts/aem.js';
-const aemxwalk = getMetadata('aemxwalk');
-console.log(`Are we delivering from crosswalk? ${aemxwalk}`);
-}
+
 
 /**
  * This function calculates the environment in which the site is running based on the URL.
